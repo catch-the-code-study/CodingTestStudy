eecea44938660554f17ce6b0a4ac55a17be60bb5
chaewon <devchae10@gmail.com>
2026-04-29 14:55:53 +0900

    solve: 문제15 요세푸스문제

---
+++ Programmers/3주차-큐/양채원/문제15_요세푸스문제.js
@@ -0,0 +1,44 @@
+/*
+  N명의 사람이 원 형태로 서 있습니다. 각 사람은 1부터 N까지 번호표를 갖고 있습니다. 그리고 임의의 숫자 K가 주어졌을 때 다음과 같이 사람을 없앱니다.
+  - 1번 번호표를 가진 사람을 기준으로 K번째 사람을 없앱니다.
+  - 없앤 사람 다음 사람을 기준으로 하고 다시 K번째 사람을 없앱니다.
+  N과 K가 주어질 때 마지막에 살아있느 사람의 번호를 반환하는 solution() 함수를 구현해주세요.
+
+  [제약조건]
+  - N과 K는 1이상 1000이하의 자연수입니다.
+*/
+
+class Queue {
+	items = [];
+	front = 0;
+	rear = 0;
+
+	push(item) {
+		this.items.push(item);
+		this.rear++;
+	}
+
+	size() {
+		return this.rear - this.front;
+	}
+
+	pop() {
+		return this.items[this.front++];
+	}
+}
+
+function solution(N, K) {
+	const queue = new Queue();
+
+	for (let i = 1; i <= N; i++) {
+		queue.push(i);
+	}
+
+	while (queue.size() > 1) {
+		for (let i = 0; i < K - 1; i++) {
+			queue.push(queue.pop());
+		}
+		queue.pop();
+	}
+	return queue.pop();
+}
