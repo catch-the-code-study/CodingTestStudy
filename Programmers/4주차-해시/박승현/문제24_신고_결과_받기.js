056475444010352e1f75ced4b08c6c3d850fa0bd
Hanpla <hanpla25@gmail.com>
2026-05-11 10:58:09 +0900

    solve: 문제24_신고 결과 받기

--- Programmers/4주차-해시/박승현/문제24_신고_결과_받기.js
+++ Programmers/4주차-해시/박승현/문제24_신고_결과_받기.js
@@ -3,6 +3,7 @@
 // id_list: ["muzi", "frodo", "apeach", "neo"]
 // report: ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
 // k: 2, result: [2,1,1,0]
+
 const solution = (id_list, report, k) => {
   const reports = [...new Set(report)];
 
